96a1c8dfc1ecc7b15c7969851574f07863dac0c6
mashiro shiina <lshinamashiro@gmail.com>
2019-11-07 13:23:54 +0800

    end

--- Assets/Scripts/SphereMove.cs
+++ Assets/Scripts/SphereMove.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SphereMove : MonoBehaviour
 {
     private Rigidbody mrb;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +17,10 @@ public class SphereMove : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-      //  mrb.AddForce(new Vector3(v, 0, -h) * Time.deltaTime*300, ForceMode.Force);
-        mrb.angularVelocity = new Vector3(-h, 0, -v) * Time.deltaTime * 300;
+        if (this.transform.CompareTag("Untagged"))
+        {
+            mrb.angularVelocity = new Vector3(-h, 0, -v) * Time.deltaTime * 300;
+        }
+        this.transform.Rotate(new Vector3(h*Time.deltaTime*20,0,v*Time.deltaTime*20));
     }
 }
